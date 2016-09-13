c814dd6725e3e0f28f485e49b593f103423af9a3
altoing <altonet@gmail.com>
2016-09-13 11:00:54 +0400

    lab2.1

--- test.py
+++ test.py
@@ -1,39 +1,17 @@
 from cgi import parse_qs, escape
 
-html = """
-<html>
-<body>
- a=%(a)s <br>
- b=%(b)s <br>
- c=%(c)s <br>
-</body>
-</html>
-"""
-
 def application (environ, start_response):
 
     # Returns a dictionary in which the values are lists
     d = parse_qs(environ['QUERY_STRING'])
 
-    for x, y in d.items():
-        print x,"=",y[0]
+    st = ""
+    for key, val in d.iteritems():
+        st = st + key + "=" + str(val[0]) + "<br>"
 
-    print d.items()
-
-    a = d.get('a', [''])[0] # Returns the first age value
-    b = d.get('b', [''])[0]
-    c = d.get('c', [''])[0]
-
-
-    response_body = html % {
-        'a': a,
-        'b': b,
-        'c': c,
-    }
-
+    response_body = st
     status = '200 OK'
 
-    # Now content type is text/html
     response_headers = [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
